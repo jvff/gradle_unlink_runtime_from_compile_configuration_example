3080612a010c89596adcb87af147e1d726b8e0b8
Janito Vaqueiro Ferreira Filho <janito.vff@@gmail.com>
2016-12-29 17:05:27 +0000

    Create compile-time library

---
+++ libnew/src/main/java/com/janitovff/test/lib/TestLib.java
@@ -0,0 +1,11 @@
+package com.janitovff.test.lib;
+
+public class TestLib {
+    public String methodA() {
+        return "New library: TestLib.methodA()";
+    }
+
+    public String methodB() {
+        return "New library: TestLib.methodB()";
+    }
+}
