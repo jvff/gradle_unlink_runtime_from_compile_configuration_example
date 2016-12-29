388bbb8a2e31ca90a0a44553721b0732483bcf17
Janito Vaqueiro Ferreira Filho <janito.vff@@gmail.com>
2016-12-29 17:06:39 +0000

    Create run-time library wrapper

---
+++ alib/src/main/java/com/janitovff/test/TestLibWrapper.java
@@ -0,0 +1,13 @@
+package com.janitovff.test.lib;
+
+public class TestLibWrapper {
+    private TestLib lib = new TestLib();
+
+    public String methodA() {
+        return lib.methodA();
+    }
+
+    public String methodB() {
+        return lib.methodB();
+    }
+}
